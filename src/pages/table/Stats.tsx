b94e755a731351ecdb471f5be277ae1a822d1fdb
Harsh Wardhan <harshwardhan847@gmail.com>
2025-05-22 16:17:54 +0530

    added colors in stats card

--- src/pages/table/Stats.tsx
+++ src/pages/table/Stats.tsx
@@ -1,6 +1,6 @@
 import type { SheetData } from "./columns";
 
-import { TrendingUpIcon } from "lucide-react";
+import { TrendingDownIcon, TrendingUpIcon } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import {
@@ -35,6 +35,8 @@ const Stats = ({ selectedRows, data }: Props) => {
         }
         return previous + curr;
       }) ?? 0;
+
+  const aggregatedScore = (totalSelectedScore / totalHelper) * 100;
   return (
     <div className="*:data-[slot=card]:shadow-xs xl:grid-cols-3 5xl:grid-cols-4 grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card">
       {/* <Card className="@container/card">
@@ -85,20 +87,51 @@ const Stats = ({ selectedRows, data }: Props) => {
         <CardHeader className="relative">
           <CardDescription>Aggregated Score</CardDescription>
           <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            {((totalSelectedScore / totalHelper) * 100)?.toFixed(2)}
+            {aggregatedScore?.toFixed(2)}%
           </CardTitle>
           <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +12.5%
+            <Badge
+              variant="outline"
+              className={`flex gap-1 rounded-lg text-xs ${
+                aggregatedScore >= 75
+                  ? "P1 border-green-500 text-green-500"
+                  : aggregatedScore >= 50
+                  ? "P2 border-amber-500 text-amber-500"
+                  : "P3 border-red-500 text-red-500"
+              }`}
+            >
+              {aggregatedScore >= 75 ? (
+                <TrendingUpIcon className="size-3" />
+              ) : aggregatedScore >= 50 ? (
+                ""
+              ) : (
+                <TrendingDownIcon className="size-3" />
+              )}
+
+              {aggregatedScore >= 75
+                ? "P1"
+                : aggregatedScore >= 50
+                ? "P2"
+                : "P3"}
             </Badge>
           </div>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <TrendingUpIcon className="size-4" />
+          <div className="line-clamp-1 flex items-center gap-2 font-medium">
+            {aggregatedScore >= 75
+              ? "🌟 High Priority"
+              : aggregatedScore >= 50
+              ? "🟡 Medium Priority"
+              : "🔴 Low Priority"}
+            {aggregatedScore >= 75 ? (
+              <TrendingUpIcon className="size-4 text-green-500" />
+            ) : aggregatedScore >= 50 ? (
+              ""
+            ) : (
+              <TrendingDownIcon className="size-4 text-red-500" />
+            )}
           </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
+          {/* <div className="text-muted-foreground">Engagement exceed targets</div> */}
         </CardFooter>
       </Card>
       {/* <Card className="@container/card">
